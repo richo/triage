a2e7d51b0b4aa690cb032e6cd65e3ba62e959a34
Luke Cawood <luke.cawood@99designs.com>
2012-03-09 11:06:04 +1100

    Added infinitescroll.js

---
+++ triage/static/js/modules/infinitescroll.js
@@ -0,0 +1,23 @@
+Triage.modules.infiniteScroll = (function($, app) {
+	"use strict";
+
+	return {
+		start: function() {
+
+			$(window).scroll(function(){
+			        if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+			           console.log('here');
+			        }
+			});
+		},
+		stop: function() {
+
+		},
+	};
+});
+
+Triage.modules.infiniteScroll.autoRegister = true;
+
+
+
+ 
